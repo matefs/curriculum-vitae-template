5676e2da75f3a469bb31326674c09c2b3b7ca35a
Mateus Schverz <matefs8569@gmail.com>
2024-12-30 19:11:04 -0300

    fix(objects): Removed unecessary

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Bem-vindo', message: 'Olá, Mundo!' });
+  res.render('index');
 });
  
 app.listen(3000, () => {
